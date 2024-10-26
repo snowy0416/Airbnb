da8a17914b9b654c2c99ce37e24f08c29f3d56ba
Soniya <soniya19106208@gmail.com>
2024-10-26 18:45:01 +0530

    navbar completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Nunito} from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
 
 
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}
+      <body className={font.className}>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
